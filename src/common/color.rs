e724064d1c41542b4c7b267bcd56bbc3028ad9cf
Lounode <lounode@gmail.com>
2021-11-14 01:54:17 +0800

    更改了在运行时溢出检查的BUG（垃圾RUST）

--- src/common/color.rs
+++ src/common/color.rs
@@ -1,19 +1,35 @@
 #[derive(Debug)]
 pub struct Color (pub u8, pub u8, pub u8);
-
+use::std::num::Wrapping;
 impl Color {
     pub fn is_same(&self, other: &Color) -> bool {
-        let dis = self.dis_2(other);
+            let dis = self.dis_2(other);
 
-        dis < 20
+            dis < 20
     }
 
     pub fn dis_2(&self, other: &Color) -> u32 {
-        let dis = (self.0 as u32 - other.0 as u32) * (self.0 as u32 - other.0 as u32)
-            + (self.1 as u32 - other.1 as u32) * (self.1 as u32 - other.1 as u32)
-            + (self.2 as u32 - other.2 as u32) * (self.2 as u32 - other.2 as u32)
-            ;
+        let sel0 = Wrapping(self.0 as u32);
+        let oth0 = Wrapping(other.0 as u32);
+        let sel1 = Wrapping(self.1 as u32);
+        let oth1 = Wrapping(other.1 as u32);
+        let sel2 = Wrapping(self.2 as u32);
+        let oth2 = Wrapping(other.2 as u32);
+
+        let dis_before = ((sel0 - oth0) * (sel0 - oth0)
+                + (sel1 - oth1) * (sel1 - oth1)
+                + (sel2 - oth2) * (sel2 - oth2)).0
+                ;
+        let dis = dis_before;
         dis
+            
+        /*
+        let dis = (self.0 as u32 - other.0 as u32) * (self.0 as u32 - other.0 as u32)
+                + (self.1 as u32 - other.1 as u32) * (self.1 as u32 - other.1 as u32)
+                + (self.2 as u32 - other.2 as u32) * (self.2 as u32 - other.2 as u32)
+                ;
+            dis
+        */
     }
 
     pub fn new() -> Color {

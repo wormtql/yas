6fef78f327bcad5c66a0f8c680617879d5956b74
poly000 <34085039+poly000@users.noreply.github.com>
2023-01-06 01:33:46 +0800

    fix build.rs

--- build.rs
+++ build.rs
@@ -1,10 +1,7 @@
 fn main() {
     if cfg!(target_os = "windows") {
         let mut res = winres::WindowsResource::new();
-        res.set_icon("test.ico")
-            .set("InternalName", "TEST.EXE")
-            // manually set version 1.0.0.0
-            .set_version_info(winres::VersionInfo::PRODUCTVERSION, 0x0001000000000000);
+        res.set_manifest_file("manifest.xml");
         res.compile().unwrap();
     }
 }

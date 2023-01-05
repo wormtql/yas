ec5d0ad3277e8dc91450bd0e0ec7809a9940ab8c
poly000 <1348292515@qq.com>
2023-01-06 00:50:24 +0800

    fix: build on linux

--- build.rs
+++ build.rs
@@ -1,9 +1,10 @@
-#[cfg(windows)]
-use winres::WindowsResource;
-
-#[cfg(windows)]
 fn main() {
-    let mut res = WindowsResource::new();
-    res.set_manifest_file("manifest.xml");
-    res.compile().unwrap();
+    if cfg!(target_os = "windows") {
+        let mut res = winres::WindowsResource::new();
+        res.set_icon("test.ico")
+            .set("InternalName", "TEST.EXE")
+            // manually set version 1.0.0.0
+            .set_version_info(winres::VersionInfo::PRODUCTVERSION, 0x0001000000000000);
+        res.compile().unwrap();
+    }
 }

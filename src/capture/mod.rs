3adc98a1531065d80ad2f7d31fb953a20a7c8300
poly000 <1348292515@qq.com>
2023-01-06 11:27:01 +0800

    fix capture_abosolute

--- src/capture/mod.rs
+++ src/capture/mod.rs
@@ -1,58 +1,44 @@
-use image::ImageBuffer;
-use tract_onnx::prelude::tract_itertools::Itertools;
-
-
 use crate::common::color::Color;
 use crate::common::PixelRect;
 
 /// retures Ok(buf) on success
 /// buf contains pixels in [b:u8, g:u8, r:u8, a:u8] format, as an `[[i32;width];height]`.
-pub fn capture_absolute(
+pub fn capture_absolute(rect: &PixelRect) -> Result<Vec<u8>, String> {
+    let mut re = Vec::with_capacity((rect.height * rect.width) as usize * 4);
+    let rgb_image = capture_absolute_image(rect)?;
+
+    let width = rect.width as usize;
+    for (i, pixel) in rgb_image.pixels().enumerate() {
+        let x = i % width;
+        let y = i / width;
+        let rgb_arr = pixel.0;
+        let offset = (y * width + x) * 4;
+        re[offset] = rgb_arr[2];
+        re[offset + 1] = rgb_arr[1];
+        re[offset + 2] = rgb_arr[0];
+        re[offset + 3] = 255;
+    }
+    Ok(re)
+}
+pub fn capture_absolute_image(
     PixelRect {
         left,
         top,
         width,
         height,
     }: &PixelRect,
-) -> Result<Vec<u8>, String> {
+) -> Result<image::RgbImage, String> {
     // simply use the first screen.
     // todo: multi-screen support
     let screen = screenshots::Screen::all().ok_or("cannot get DisplayInfo")?[0];
-    let mut rgba_buf = screen
+    let png = screen
         .capture_area(*left, *top, *width as u32, *height as u32)
-        .ok_or("capture failed")?
-        .buffer().clone();
-    
-    for mut rgba in &rgba_buf.iter_mut().chunks(4) {
-        let r = rgba.nth(0).ok_or("cannot access r in rgba chunk")?;
-        let b = rgba.nth(2).ok_or("cannot access b in rgba chunk")?;
-        std::mem::swap(r, b);
+        .ok_or("capture failed")?;
+    let rgba_image = image::load_from_memory_with_format(png.buffer(), image::ImageFormat::Png);
+    match rgba_image {
+        Ok(dyc_img) => Ok(dyc_img.to_rgb8()),
+        Err(e) => Err("Error occured on image loading".into()),
     }
-
-    Ok(rgba_buf)
-}
-
-pub fn capture_absolute_image(rect: &PixelRect) -> Result<image::RgbImage, String> {
-    let raw: Vec<u8> = match capture_absolute(rect) {
-        Err(s) => {
-            return Err(s);
-        }
-        Ok(v) => v,
-    };
-
-    let height = rect.height as u32;
-    let width = rect.width as u32;
-
-    let mut img = ImageBuffer::from_fn(width, height, move |x, y| {
-        let y = height - y - 1;
-        let offset = (y * width + x) * 4;
-        let b = raw[(offset + 0) as usize];
-        let g = raw[(offset + 1) as usize];
-        let r = raw[(offset + 2) as usize];
-        image::Rgb([r, g, b])
-    });
-
-    Ok(img)
 }
 
 pub fn get_color(x: u32, y: u32) -> Color {
@@ -63,9 +49,5 @@ pub fn get_color(x: u32, y: u32) -> Color {
         height: 1,
     })
     .unwrap();
-
-    let b = im[0];
-    let g = im[1];
-    let r = im[2];
-    Color(r, g, b)
+    Color::from(im[2], im[1], im[0])
 }

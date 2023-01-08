5f3d7ad574545ed55bedc21e5e00b9409961bc5b
poly000 <1348292515@qq.com>
2023-01-09 02:17:00 +0800

    prevent unnessacary PNG decode/encode

--- src/capture/mod.rs
+++ src/capture/mod.rs
@@ -1,21 +1,32 @@
+use image::{Rgb, RgbImage};
+
 use crate::common::color::Color;
 use crate::common::PixelRect;
 
 /// retures Ok(buf) on success
 /// buf contains pixels in [b:u8, g:u8, r:u8, a:u8] format, as an `[[i32;width];height]`.
-pub fn capture_absolute(rect: &PixelRect) -> Result<Vec<u8>, String> {
-    let mut re = Vec::with_capacity((rect.height * rect.width) as usize * 4);
-    let rgb_image = capture_absolute_image(rect)?;
+pub fn capture_absolute(
+    PixelRect {
+        left,
+        top,
+        width,
+        height,
+    }: &PixelRect,
+) -> Result<Vec<u8>, String> {
+    let screen = screenshots::Screen::all().ok_or("cannot get DisplayInfo")?[0];
+    let (mut buffer, is_rgba) = screen
+        .capture_area(*left, *top, *width as u32, *height as u32)
+        .ok_or("capture failed")?;
 
-    for pixel in rgb_image.pixels() {
-        let rgb_arr = pixel.0;
-
-        re.push(rgb_arr[2]);
-        re.push(rgb_arr[1]);
-        re.push(rgb_arr[0]);
-        re.push(255);
+    if is_rgba {
+        for chunk in buffer.chunks_mut(4) {
+            let temp = chunk[0];
+            chunk[0] = chunk[2];
+            chunk[2] = temp;
+        }
     }
-    Ok(re)
+
+    Ok(buffer)
 }
 
 pub fn capture_absolute_image(
@@ -29,14 +40,17 @@ pub fn capture_absolute_image(
     // simply use the first screen.
     // todo: multi-screen support
     let screen = screenshots::Screen::all().ok_or("cannot get DisplayInfo")?[0];
-    let png = screen
+    let (buffer, is_rgba) = screen
         .capture_area(*left, *top, *width as u32, *height as u32)
         .ok_or("capture failed")?;
-    let rgba_image = image::load_from_memory_with_format(png.buffer(), image::ImageFormat::Png);
-    match rgba_image {
-        Ok(dyc_img) => Ok(dyc_img.to_rgb8()),
-        Err(_) => Err("Error occured on image loading".into()),
-    }
+    Ok(RgbImage::from_fn(*width as u32, *height as u32, |x, y| {
+        let offset = (y * (*width as u32) + x) as usize;
+        if is_rgba {
+            Rgb([buffer[offset], buffer[offset + 1], buffer[offset + 2]])
+        } else {
+            Rgb([buffer[offset + 2], buffer[offset + 1], buffer[offset]])
+        }
+    }))
 }
 
 pub fn get_color(x: u32, y: u32) -> Color {
